ddf7588d52e701887cf63fc3523326fc8901b83f
George Allaway <georgeallaway@georges-air.home>
2022-05-18 19:12:05 +0100

    chore: add example package and reqs

---
+++ my_package/foo.py
@@ -0,0 +1,25 @@
+def add(a: int, b: int) -> int:
+    """Returns sum of a and b.
+
+    Args:
+        a (int)
+        b (int)
+
+    Returns:
+        int: addition of a and b
+
+    """
+    return a + b
+
+
+def subtract(a: int, b: int):
+    """Returns value of b subtracted from a.
+
+    Args:
+        a (int)
+        b (int)
+
+    Returns:
+        int: a minus b
+    """
+    return a - b
